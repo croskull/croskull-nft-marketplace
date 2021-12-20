f30d2cea5fa7a0480f720ea07f953263c1fc6ee4
Alessandro De Cristofaro <hiutaky@gmail.com>
2021-12-20 22:23:33 +0100

    new depoloy

--- precache-manifest.e98ae55feda931f81e4f8ff35617e544.js
+++ precache-manifest.e98ae55feda931f81e4f8ff35617e544.js
@@ -1,38 +1,38 @@
 self.__precacheManifest = [
   {
     "revision": "7961e54088c3fdbc924d",
-    "url": "/cryptoboys-nft-marketplace/static/css/main.ad901c2e.chunk.css"
+    "url": "/croskull-nft-marketplace/static/css/main.ad901c2e.chunk.css"
   },
   {
     "revision": "7961e54088c3fdbc924d",
-    "url": "/cryptoboys-nft-marketplace/static/js/main.7961e540.chunk.js"
+    "url": "/croskull-nft-marketplace/static/js/main.7961e540.chunk.js"
   },
   {
     "revision": "991160fbcff757b39130",
-    "url": "/cryptoboys-nft-marketplace/static/js/1.991160fb.chunk.js"
+    "url": "/croskull-nft-marketplace/static/js/1.991160fb.chunk.js"
   },
   {
     "revision": "8bf4deafcc65d566b658",
-    "url": "/cryptoboys-nft-marketplace/static/js/runtime~main.8bf4deaf.js"
+    "url": "/croskull-nft-marketplace/static/js/runtime~main.8bf4deaf.js"
   },
   {
     "revision": "ba81d5f2c0d6645c45b6212eb2a8078b",
-    "url": "/cryptoboys-nft-marketplace/static/media/1.ba81d5f2.png"
+    "url": "/croskull-nft-marketplace/static/media/1.ba81d5f2.png"
   },
   {
     "revision": "26ef8bb418031b9bb4f44e1aeea71186",
-    "url": "/cryptoboys-nft-marketplace/static/media/loading.26ef8bb4.gif"
+    "url": "/croskull-nft-marketplace/static/media/loading.26ef8bb4.gif"
   },
   {
     "revision": "fbf33967fa244d21d61fb85f233fc331",
-    "url": "/cryptoboys-nft-marketplace/static/media/metamask.fbf33967.svg"
+    "url": "/croskull-nft-marketplace/static/media/metamask.fbf33967.svg"
   },
   {
     "revision": "bcd172661f5eec582912d26c3d1fb850",
-    "url": "/cryptoboys-nft-marketplace/static/media/logo-cr-skull-white.bcd17266.png"
+    "url": "/croskull-nft-marketplace/static/media/logo-cr-skull-white.bcd17266.png"
   },
   {
     "revision": "731429d074feb31fc76528707b5a28bc",
-    "url": "/cryptoboys-nft-marketplace/index.html"
+    "url": "/croskull-nft-marketplace/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cryptoboys-nft-marketplace/precache-manifest.e98ae55feda931f81e4f8ff35617e544.js"
+  "/croskull-nft-marketplace/precache-manifest.e98ae55feda931f81e4f8ff35617e544.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cryptoboys-nft-marketplace/index.html", {
+workbox.routing.registerNavigationRoute("/croskull-nft-marketplace/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
