56e5766a1c6f20382308b25282ebd98596b13d44
tiapezz <mattia.pezzolla.mobile@gmail.com>
2021-12-22 10:23:26 +0100

    fix page metamask and cassini

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -1,43 +1,47 @@
 import React, { useEffect } from "react";
 import metamaskIcon from "./metamask.svg";
+import './ConnectToMetamask.css';
 
 
-const ConnectToMetamask = ( { connectToMetamask, connectToWalletConnect } ) => {
-  
+const ConnectToMetamask = ({ connectToMetamask, connectToWalletConnect }) => {
+
 
   return (
-    <div className="jumbotron">
-      <h1 className="display-5">
-        CROSkull NFT Marketplace
-      </h1>
-      <p className="lead">
-        Access CRSkull Metaverse
-      </p>
-      <hr className="my-4" />
-      <button
-        onClick={connectToMetamask}
-        className="btn btn-primary d-flex align-items-center"
-        style={{ fontSize: "0.9rem", letterSpacing: "0.14rem" }}
-      >
-        Connect Metamask{" "}
-        <img
-          src={metamaskIcon}
-          alt="metamask-icon"
-          style={{ width: "2rem", marginLeft: "0.5rem" }}
-        />
-      </button>
-      <button
-        onClick={connectToWalletConnect}
-        className="btn btn-primary d-flex align-items-center"
-        style={{ fontSize: "0.9rem", letterSpacing: "0.14rem" }}
-      >
-        Connect to WalletConnect{" "}
-        <img
-          src={metamaskIcon}
-          alt="metamask-icon"
-          style={{ width: "2rem", marginLeft: "0.5rem" }}
-        />
-      </button>
+    <div className="container metamask">
+      <div className="">
+        <h1 className="display-5">
+          CROSkull NFT Marketplace
+        </h1>
+        <p className="lead">
+          Access CRSkull Metaverse
+        </p>
+        <hr className="my-4" />
+        <button
+          onClick={connectToMetamask}
+          className="btn btn-primary d-flex align-items-center"
+          style={{ fontSize: "0.9rem", letterSpacing: "0.14rem" }}
+        >
+          Connect Metamask{" "}
+          <img
+            src={metamaskIcon}
+            alt="metamask-icon"
+            style={{ width: "2rem", marginLeft: "0.5rem" }}
+          />
+        </button>
+        <br></br>
+        <button
+          onClick={connectToWalletConnect}
+          className="btn btn-primary d-flex align-items-center"
+          style={{ fontSize: "0.9rem", letterSpacing: "0.14rem" }}
+        >
+          Connect to WalletConnect{" "}
+          <img
+            src={metamaskIcon}
+            alt="metamask-icon"
+            style={{ width: "2rem", marginLeft: "0.5rem" }}
+          />
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/ContractNotDeployed/ContractNotDeployed.jsx
+++ src/components/ContractNotDeployed/ContractNotDeployed.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ContractNotDeployed = () => {
   return (
-    <div className="jumbotron">
+    <div className="container metamask">
       <h3>CRSkull Contract Not Deployed To This Network.</h3>
       <hr className="my-4" />
       <p className="lead">
